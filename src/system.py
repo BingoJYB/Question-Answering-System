f4c1f23844fb630534279c43433e50bc4ef6a1fb
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-09-05 18:28:59 +0200

    Update system.py

--- src/system.py
+++ src/system.py
@@ -1,7 +1,19 @@
-from preprocessing import Preprocessing
+from aqprocessor import AQprocessor
+from analyzer import Analyzer
+from collections import Counter
 from data import answers
 
+aqprocessor = AQprocessor(answers)
+analyzer = Analyzer()
+question = aqprocessor.process_question("When do I crack the egg into the pan?")
 
-preprocessor = Preprocessing()
-text = answers['method 1']['step 1']
-print(preprocessor.stopword_removal(preprocessor.text_segmentation(text)))
+for methodID, steps in answers.items():
+    texts_lemmatized = aqprocessor.process_answer(steps)
+
+    vec1 = Counter(question)
+    for text in texts_lemmatized:
+        vec2 = Counter(text)
+        cosine = analyzer.get_cosine(vec1, vec2)
+        print(cosine)
+
+    print('==================================================================================')
